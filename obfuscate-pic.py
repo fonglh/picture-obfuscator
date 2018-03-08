68fc2fceab32e6f00ae91bef0d58043ef16d22ae
fonglh <fonglh@gmail.com>
2018-03-08 13:55:59 +0800

    Add mode to generate all possible pictures.
    If the number of bozzcoins is not specified on the command line,
    generate all possible pictures.

--- obfuscate-pic.py
+++ obfuscate-pic.py
@@ -31,11 +31,18 @@ def draw_cell(image_drawer, top_left_coordinate, size):
     inner_bottom_right = (bottom_right_coordinate[0] - BORDER_THICKNESS, bottom_right_coordinate[1] - BORDER_THICKNESS)
     image_drawer.rectangle((inner_top_left, inner_bottom_right), fill=CELL_COLOUR)
 
+def obfuscate_picture(image_drawer, covered_cells, parameters):
+    for cell_number in covered_cells:
+        overlay_cell(image_drawer, cell_number, parameters)
+
 if len(sys.argv) == 3:
     picture_filename = sys.argv[1]
     bozzcoins = int(sys.argv[2])
+elif len(sys.argv) == 2:        # Generate all possible pictures
+    picture_filename = sys.argv[1]
+    bozzcoins = None
 else:
-    print('Usage: obfuscate-pic.py <picture_filename> <bozzcoins>')
+    print('Usage: obfuscate-pic.py <picture_filename> [<bozzcoins>]')
     exit(1)
 
 image = Image.open(picture_filename)
@@ -49,13 +56,20 @@ draw = ImageDraw.Draw(image)
 # Calculate total number of cells then figure out how many cells should be covered.
 num_total_cells = parameters['cell_counts'][0] * parameters['cell_counts'][1]
 num_coins_per_cell = parameters['target'] // num_total_cells
-num_covered_cells = num_total_cells - bozzcoins // num_coins_per_cell
 
-# Fix the random seed so the choice of cells to cover is always the same.
-random.seed(parameters['random_seed'])
-covered_cells = random.sample(range(num_total_cells), num_covered_cells)
-
-for cell_number in covered_cells:
-    overlay_cell(draw, cell_number, parameters)
-
-image.save(picture_filename.split('.')[0] + '-obfuscated.jpg')
+if bozzcoins is None:
+    for i in range(num_total_cells + 1):
+        image = Image.open(picture_filename)
+        draw = ImageDraw.Draw(image)
+        # Need to reset the seed each time so the newly covered cell is 1 more than the last picture
+        random.seed(parameters['random_seed'])
+        covered_cells = random.sample(range(num_total_cells), i)
+        obfuscate_picture(draw, covered_cells, parameters)
+        image.save(picture_filename.split('.')[0] + '-' + str(num_total_cells - i) + '.jpg')
+else:
+    # Fix the random seed so the choice of cells to cover is always the same.
+    random.seed(parameters['random_seed'])
+    num_covered_cells = num_total_cells - bozzcoins // num_coins_per_cell
+    covered_cells = random.sample(range(num_total_cells), num_covered_cells)
+    obfuscate_picture(draw, covered_cells, parameters)
+    image.save(picture_filename.split('.')[0] + '-obfuscated.jpg')
